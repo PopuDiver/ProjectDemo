df3632a65d84580b0891e70c6948416e73e59acb
PopuDiver <2982500540@qq.com>
2023-02-20 12:24:09 +0800

    见缝插针小游戏

---
+++ 见缝插针小游戏/stickPinProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private Transform startPoint;
+    private Transform spawnPoint;
+    private Pin currentPin;
+    public Text scoreText;
+    public float speed = 3;
+    public int score = 0;
+    public bool isGameOver;
+    public GameObject pinPrefab;
+    private Camera mainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+        mainCamera = Camera.main;
+        SpawnPin();
+    }
+
+    private void Update()
+    {
+        if (isGameOver) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            score++;
+            scoreText.text = score.ToString();
+
+
+            currentPin.StartFly();
+            SpawnPin();
+        }
+    }
+
+    void SpawnPin()
+    {
+        currentPin =  GameObject.Instantiate(pinPrefab, spawnPoint.position, pinPrefab.transform.rotation).GetComponent<Pin>();
+    }
+
+    public void GameOver()
+    {
+        if (isGameOver) return;
+        GameObject.Find("Circle").GetComponent<circleController>().enabled = false;
+        StartCoroutine(GameOverAnimation());
+        isGameOver = true;
+    }
+
+    IEnumerator GameOverAnimation()
+    {
+        while (true)
+        {
+            mainCamera.backgroundColor = Color.Lerp(mainCamera.backgroundColor, Color.red, speed * Time.deltaTime);
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 4, speed * Time.deltaTime);
+            if(Mathf.Abs(mainCamera.orthographicSize - 4) < 0.01f)
+            {
+                break;
+            }
+            yield return 0;
+        }
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ 见缝插针小游戏/stickPinProject/Assets/Scripts/Pin.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pin : MonoBehaviour
+{
+    public float speed = 5;
+    private bool isFly = false;
+    private bool isReach = false;
+    private Transform startPoint;
+    private Transform circle;
+    private Vector3 targetCirclePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.Find("Circle").transform;
+        targetCirclePos = circle.position;
+        targetCirclePos.y -= 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFly == false)
+        {
+            if (isReach == false)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+                if(Vector3.Distance(transform.position,startPoint.position) < 0.05f)
+                {
+                    isReach = true;
+                }
+            }
+
+        }
+        
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetCirclePos, speed * Time.deltaTime);
+            if(Vector3.Distance(transform.position,targetCirclePos) < 0.05f)
+            {
+                transform.position = targetCirclePos;
+                transform.parent = circle;
+
+                isFly = false;
+            }
+        }
+    }
+
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+    }
+}
